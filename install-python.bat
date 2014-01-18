ddbac047a76755db040b8e8c16fe706e0d1f4803
Max Vilimpoc <max@vilimpoc.org>
2014-01-17 18:57:29 -0600

    Updated install to include easy_install, pip, and virtualenv.

--- install-python.bat
+++ install-python.bat
@@ -25,7 +25,7 @@ echo,
 
 if not exist %PYTHON_EXE% (
 if exist %PYTHON_MSI% (
-    msiexec.exe /i %PYTHON_MSI% ALLUSERS=1 ADDDEFAULT=ALL
+    msiexec.exe /qb /i %PYTHON_MSI% ALLUSERS=1 ADDLOCAL=ALL
 )
 )
 
@@ -38,5 +38,34 @@ echo,
 rem reg add "HKCU\Environment" /v PATH /t REG_EXPAND_SZ /d "%PYTHON_PATH%"
 rem reg add "HKLM\SYSTEM\ControlSet001\Control\Session Manager\Environment" /v PATH /t REG_EXPAND_SZ /d "%PATH%;c:\Python27;c:\Python27\Scripts"
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v PATH /t REG_EXPAND_SZ /d "%PATH%;%PYTHON_PATH%"
+
+rem Temporarily set the path.
+set PATH=%PATH%;%PYTHON_PATH%
+
+echo,
+echo ------------------------------------------------------------------
+echo Add easy_install
+echo ------------------------------------------------------------------
+echo,
+
+curl -O https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py
+python ez_setup.py
+
+echo,
+echo ------------------------------------------------------------------
+echo Add pip
+echo ------------------------------------------------------------------
+echo,
+
+curl -O https://raw.github.com/pypa/pip/master/contrib/get-pip.py
+python get-pip.py
+
+echo,
+echo ------------------------------------------------------------------
+echo Add virtualenv
+echo ------------------------------------------------------------------
+echo,
+
+pip install virtualenv
 
 
