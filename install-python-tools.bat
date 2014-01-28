e1196cab2fa4c7447319fe3de4e8afd21c20cc22
Max <max@vilimpoc.org>
2014-01-28 23:30:31 +0100

    Create install-python-tools.bat

---
+++ install-python-tools.bat
@@ -0,0 +1,22 @@
+echo,
+echo ------------------------------------------------------------------
+echo Run this as a non-admin user.
+echo
+echo Non-global install. For some reason virtualenv has problems if 
+echo you install it as an admin and use it as a non-admin.
+echo ------------------------------------------------------------------
+echo,
+
+echo,
+echo ------------------------------------------------------------------
+echo Add virtualenv
+echo ------------------------------------------------------------------
+echo,
+
+pip install virtualenv --user
+
+echo,
+echo ------------------------------------------------------------------
+echo virtualenv is installed!
+echo ------------------------------------------------------------------
+echo,
